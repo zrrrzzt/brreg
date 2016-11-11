5d4b1fb3ea57d1c95762cfff2d785c5ec60775cc
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-11 21:20:06 +0100

    Adds promises interface

--- index.js
+++ index.js
@@ -3,26 +3,42 @@
 const difi = require('difi')
 
 module.exports = (options, callback) => {
-  if (!options) {
-    return callback(new Error('Missing required input: options'), null)
-  }
-  if (!options.query) {
-    return callback(new Error('Missing required input: options.query'), null)
-  }
-
-  const difiOptions = {
-    dataset: 'brreg/enhetsregisteret',
-    format: options.format || 'json',
-    query: {
-      query: options.query
+  return new Promise((resolve, reject) => {
+    if (!options) {
+      let error = new Error('Missing required input: options')
+      if (callback) {
+        return callback(error, null)
+      }
+      reject(error)
     }
-  }
-
-  difi(difiOptions, (error, data) => {
-    if (error) {
-      return callback(error, null)
-    } else {
-      return callback(null, data)
+    if (!options.query) {
+      let error = new Error('Missing required input: options.query')
+      if (callback) {
+        return callback(error, null)
+      }
+      reject(error)
     }
+
+    const difiOptions = {
+      dataset: 'brreg/enhetsregisteret',
+      format: options.format || 'json',
+      query: {
+        query: options.query
+      }
+    }
+
+    difi(difiOptions, (error, data) => {
+      if (error) {
+        if (callback) {
+          return callback(error, null)
+        }
+        reject(error)
+      } else {
+        if (callback) {
+          return callback(null, data)
+        }
+        resolve(data)
+      }
+    })
   })
 }
